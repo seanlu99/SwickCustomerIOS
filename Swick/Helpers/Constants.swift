9791819f40fb68f5c7bcb15b463950acb2c65d70
Andrew Jiang <andrewjiang@Andrews-MacBook-Pro.local>
2020-10-21 18:44:26 -0400

    Remove customer and server folders

--- Swick/Helpers/Constants.swift
+++ Swick/Helpers/Constants.swift
@@ -12,10 +12,7 @@ let BACKEND_URL = DEVELOPMENT ? "http://localhost:8000" : "https://swickapp.hero
 
 #if CUSTOMER
 // Mock scanned string for QR code scanner while using simulator
-let MOCK_SCANNED_STRING = "swick-6-1"
+let MOCK_SCANNED_STRING = "swick-1-1"
 
 let STRIPE_PUBLIC_KEY = "pk_test_YoEmowu4ykyrOgUO8dfmShpQ00ck79Hp8Q"
-
-// Time in seconds of how long customers can send a tip on an order
-let TIP_GRACE_PERIOD = 259200
 #endif
